185c42e7da5159709bdb6670526d6b738fb7e065
Abhishek Das <>
2024-10-15 16:56:36 +0530

    updated simple rag notebook

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,3 @@
-
 def replace_t_with_space(documents):
     """
     Replaces all tab characters ('\t') with spaces in the page content of each document.
@@ -13,3 +12,39 @@ def replace_t_with_space(documents):
     for doc in documents:
         doc.page_content = doc.page_content.replace("\t", " ")
     return documents
+
+def retrieve_context_per_question(question, chunks_query_retriever):
+    """
+    Retrieves relevant context and unique URLs for a given question using the chunks query retriever.
+
+    Args:
+        question: The question for which to retrieve context and URLs.
+
+    Returns:
+        A tuple containing:
+        - A string with the concatenated content of relevant documents.
+        - A list of unique URLs from the metadata of the relevant documents.
+    """
+
+    # Retrieve relevant documents for the given question
+    docs = chunks_query_retriever.invoke(question)
+
+    # Concatenate document content
+    # context = " ".join(doc.page_content for doc in docs)
+    context = [doc.page_content for doc in docs]
+
+    return context
+
+def show_context(context):
+    """
+    Display the contents of the provided context list.
+
+    Args:
+        context (list): A list of context items to be displayed.
+
+    Prints each context item in the list with a heading indicating its position.
+    """
+    for i, c in enumerate(context):
+        print(f"Context {i + 1}:")
+        print(c)
+        print("\n")
